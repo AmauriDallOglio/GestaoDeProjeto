4212d69875b3ef900068525c2ce3ce5a4cf751be
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2024-02-24 18:59:39 -0300

    Ajustes do profile da empresa

--- GestaoDeProjeto.Aplicacao/Profile/EmpresaProfile.cs
+++ GestaoDeProjeto.Aplicacao/Profile/EmpresaProfile.cs
@@ -11,6 +11,14 @@ namespace GestaoDeProjeto.Aplicacao
             CreateMap<Empresa, EmpresaIncluirRequest>().ReverseMap();
             CreateMap<Empresa, EmpresaIncluirResponse>().ReverseMap();
 
+            CreateMap<Empresa, EmpresaAlterarRequest>().ReverseMap();
+            CreateMap<Empresa, EmpresaAlterarResponse>().ReverseMap();
+
+
+            CreateMap<Empresa, EmpresaExcluirRequest>().ReverseMap();
+            CreateMap<Empresa, EmpresaExcluirResponse>().ReverseMap();
+
+
             CreateMap<Empresa, EmpresaObterTodosRequest>().ReverseMap();
             CreateMap<Empresa, EmpresaObterTodosResponse>().ReverseMap();
         }
