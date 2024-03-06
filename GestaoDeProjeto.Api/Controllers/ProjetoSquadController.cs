475053e530fc01d55955f2c94b2d92aeebea16c4
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2024-03-06 20:42:40 -0300

    Projeto squad incluir

---
+++ GestaoDeProjeto.Api/Controllers/ProjetoSquadController.cs
@@ -0,0 +1,34 @@
+﻿using GestaoDeProjeto.Aplicacao.Command.Projetos;
+using GestaoDeProjeto.Aplicacao.DML.ProjetoSquads;
+using GestaoDeProjeto.Dominio.Util;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeProjeto.Api.Controllers
+{
+    [Route("api/v1/ProjetoSquad")]
+    [ApiController]
+    public class ProjetoSquadController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ProjetoSquadController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("Conexao"), ActionName("Conexao")]
+        public string Conexao()
+        {
+            return "Ok";
+        }
+
+        [HttpPost("Inserir"), ActionName("Inserir")]
+        public async Task<ResultadoOperacao<ProjetoSquadIncluirResponse>> Inserir([FromBody] ProjetoSquadIncluirRequest request)
+        {
+            ResultadoOperacao<ProjetoSquadIncluirResponse> response = await _mediator.Send(request);
+            return response;
+        }
+
+
+    }
+}
---
+++ GestaoDeProjeto.Aplicacao/DML/ProjetoSquads/ProjetoSquadIncluir.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using GestaoDeProjeto.Dominio.Entidade;
+using GestaoDeProjeto.Dominio.InterfaceRepositorio;
+using GestaoDeProjeto.Dominio.Util;
+using MediatR;
+
+namespace GestaoDeProjeto.Aplicacao.DML.ProjetoSquads
+{
+    public class ProjetoSquadIncluirRequest : IRequest<ResultadoOperacao<ProjetoSquadIncluirResponse>>
+    {
+        public int Id_Projeto { get; set; }
+        public int Id_Squad { get; set; }
+        public bool Inativo { get; set; }
+
+    }
+
+    public class ProjetoSquadIncluirResponse
+    {
+        public int Id { get; set; }
+    }
+
+    public class ProjetoSquadIncluirHandler : IRequestHandler<ProjetoSquadIncluirRequest, ResultadoOperacao<ProjetoSquadIncluirResponse>>
+    {
+        private readonly IProjetoSquadRepositorio _iProjetoSquadRepositorio;
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public ProjetoSquadIncluirHandler(IMediator mediator, IMapper mapper, IProjetoSquadRepositorio repository)
+        {
+            _iProjetoSquadRepositorio = repository;
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        public Task<ResultadoOperacao<ProjetoSquadIncluirResponse>> Handle(ProjetoSquadIncluirRequest request, CancellationToken cancellationToken)
+        {
+
+            ProjetoSquad entidade = _mapper.Map<ProjetoSquad>(request);
+
+            entidade = _iProjetoSquadRepositorio.Inserir(entidade, true);
+
+            ProjetoSquadIncluirResponse response = _mapper.Map<ProjetoSquadIncluirResponse>(entidade);
+
+            Task<ResultadoOperacao<ProjetoSquadIncluirResponse>> sucesso = ResultadoOperacao<ProjetoSquadIncluirResponse>.RetornaSuccessoAsync(response, "Projeto", "Sucesso");
+            return sucesso;
+        }
+    }
+}
